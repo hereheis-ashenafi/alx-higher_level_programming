ee861fd524e8e3425ad7b632ae0b247ecfca6c15
አሸናፊ ካሳሁን ነጋሽ <105202844+hereheis-ashenafi@users.noreply.github.com>
2022-12-02 11:21:57 +0300

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,31 +1,18 @@
 #!/usr/bin/python3
-""" This module filters all the states from the
-    database hbtn_0e_0_usa that start with upper N.
+"""script that lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa
 """
-
+from sys import argv
 import MySQLdb
-import sys
 
 
-def main():
-    """
-        Function containing code to filter all the states
-        from the database.
-    """
-
-    # Create a database connection
-    conn = MySQLdb.connect(
-                host="localhost", port=3306, user=sys.argv[1],
-                passwd=sys.argv[2], db=sys.argv[3], charset="utf8mb4"
-            )
-    cur = conn.cursor()
-    # Select states
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    [print(state) for state in query_rows if state[1][0] == "N"]
-    cur.close()
-    conn.close()
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    user, password, database = argv[1], argv[2], argv[3]
+    db = MySQLdb.connect(host="localhost", user=user,
+                         passwd=password, db=database)
+    db = db.cursor()
+    db.execute("""SELECT * FROM states
+    WHERE REGEXP_LIKE(name, '^N', 'c') ORDER BY id""")
+    r = db.fetchall()
+    for i in r:
+        print(i)
