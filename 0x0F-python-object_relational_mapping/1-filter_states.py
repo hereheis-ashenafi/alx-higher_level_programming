be78103bde206958691f0503f35034c45391a974
አሸናፊ ካሳሁን ነጋሽ <105202844+hereheis-ashenafi@users.noreply.github.com>
2022-12-02 10:57:08 +0300

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,31 @@
--- Create states table in hbtn_0e_0_usa with some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
-USE hbtn_0e_0_usa;
-CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+#!/usr/bin/python3
+""" This module filters all the states from the
+    database hbtn_0e_0_usa that start with upper N.
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """
+        Function containing code to filter all the states
+        from the database.
+    """
+
+    # Create a database connection
+    conn = MySQLdb.connect(
+                host="localhost", port=3306, user=sys.argv[1],
+                passwd=sys.argv[2], db=sys.argv[3], charset="utf8mb4"
+            )
+    cur = conn.cursor()
+    # Select states
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    [print(state) for state in query_rows if state[1][0] == "N"]
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
